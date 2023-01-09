5154f580713349e70dc98f8be8f425012ad300da
Ashenafi Yacob <asheimade@gmail.com>
2023-01-09 04:19:12 -0800

    fix error

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     res_list = []
     for i in my_list:
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             res_list.insert(True)
         else:
             res_list.insert(False)
