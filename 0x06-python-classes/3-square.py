36f5cac154e768607a9fc08223b964d3c5120383
AshuIMade <asheimade@gmail.com>
2023-01-24 13:01:33 -0800

    fix error

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
-
 """Class Square."""
 
 
 class Square:
-
-
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -14,6 +11,5 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    
     def area(self):
         return (self.__size * self.__size)
