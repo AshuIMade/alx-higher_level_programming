f59550b63d66d51e64f86096c7b3ed08b22f5230
AshuIMade <asheimade@gmail.com>
2023-01-24 12:44:16 -0800

    fix error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 
-""""Class Square"""
+"""Class Square."""
 
 
 class Square:
-
-    def __init__(self, size = 0):
+    """Represent a square."""
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size value must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size
