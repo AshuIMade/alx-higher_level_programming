e7d869a0d44862812be7665b125b507ce778e248
AshuIMade <asheimade@gmail.com>
2023-01-24 06:01:26 -0800

    attribute instantiation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+""""Class Square"""
+
+
+class Square:
+
+    def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size value must be >=0")
+        self.__size = size
