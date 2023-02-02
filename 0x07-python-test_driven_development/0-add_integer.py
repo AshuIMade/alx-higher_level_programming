799f382e176e8bcb22926d4aa739bc27e3383e77
AshuIMade <asheimade@gmail.com>
2023-02-01 23:36:32 -0800

    fix error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,12 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
+
+"""Defines an integer addition function."""
+
+
 def add_integer(a, b=98):
-    if type[a] is not [int, float]:
+    """Integer addition"""
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if type[b] is not [int, float]:
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    return a + b
+    return (int(a) + int(b))
