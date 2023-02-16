942248441bd3273397e0ab8acd920f330fc5ce8b
AshuIMade <asheimade@gmail.com>
2023-02-16 04:49:17 -0800

    fix error

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -21,3 +21,6 @@ class TestBase_instantiation(unittest.TestCase):
         b2 = Base()
         b3 = Base()
         self.assertEqual(b1.id, b2.id -2)
+
+if __name__ == "__main__":
+    unittest.main()
